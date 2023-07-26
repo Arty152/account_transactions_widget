33a64a23e632cbdb0d9886bad7f8fe2900dd7590
Andrey Zhirkov <zhirkov.arty@gmail.com>
2023-07-26 14:36:03 +0300

    edited variable names

--- account_transactions_widget/main.py
+++ account_transactions_widget/main.py
@@ -8,37 +8,40 @@ def display_last_5_operations():
     for operation in last_operations:
         operation_date = operation['date'][:10]
         date = operation_date[-2:] + '.' + operation_date[5:7] + '.' + operation_date[:4]
-        operation_description = operation['description']
-
+        description = operation['description']
+        number_to = operation['to'].split()[-1]
+        name_check_to = operation['to'].split()[0]
+        amount = operation['operationAmount']['amount']
+        currency = operation['operationAmount']['currency']['name']
+        from_list = []
         if 'from' in operation:
-            operation_from = operation['from'].split()[-1]
-            prop_name_card = operation['from'].split()[0]
+            from_list.append(operation['from'])
+            number_from = operation['from'].split()[-1]
+            for item in from_list:
+                if len(item.split()) == 3:
+                    card_name = item.split()[:2]
+                    card_name = ' '.join(card_name)
+                else:
+                    card_name = operation['from'].split()[0]
 
-            if len(operation_from) == 16:
-                secure_number_from = operation_from[:6] + len(operation_from[6:-4]) * '*' + operation_from[-4:]
+            if len(number_from) == 16:
+                secure_number_from = number_from[:6] + len(number_from[6:-4]) * '*' + number_from[-4:]
                 secure_number_split_from = re.sub(r'.{4}', r'\g<0> ', secure_number_from)
             else:
-                secure_number_split_from = len(operation_from[-6:-4]) * '*' + operation_from[-4:]
+                secure_number_split_from = len(number_from[-6:-4]) * '*' + number_from[-4:]
 
         else:
-            prop_name_card = 'Открытие'
+            card_name = 'Открытие'
             secure_number_split_from = 'нового счета'
 
-        operation_to = operation['to'].split()[-1]
-        prop_name_check = operation['to'].split()[0]
-
-        if len(operation_to) == 16:
-            secure_number_to = operation_to[:6] + len(operation_to[6:-4]) * '*' + operation_to[-4:]
+        if len(number_to) == 16:
+            secure_number_to = number_to[:6] + len(number_to[6:-4]) * '*' + number_to[-4:]
             secure_number_split_to = re.sub(r'.{4}', r'\g<0> ', secure_number_to)
         else:
-            secure_number_split_to = len(operation_to[-6:-4]) * '*' + operation_to[-4:]
+            secure_number_split_to = len(number_to[-6:-4]) * '*' + number_to[-4:]
 
-        operation_amount = operation['operationAmount']['amount']
-        operation_currency = operation['operationAmount']['currency']['name']
-
-        print(f"{date} {operation_description}")
-        print(f"{prop_name_card} {secure_number_split_from} -> {prop_name_check} {secure_number_split_to}")
-        print(f"{operation_amount} {operation_currency}\n")
-
+        print(f"{date} {description}")
+        print(f"{card_name} {secure_number_split_from} -> {name_check_to} {secure_number_split_to}")
+        print(f"{amount} {currency}\n")
 
 display_last_5_operations()
